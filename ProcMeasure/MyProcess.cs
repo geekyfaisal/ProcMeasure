b82ac644370de5bf42d3e7176cc2b571e42cc7d6
Faisal Andrabi <geekyfaisal@outlook.com>
2024-08-09 10:38:33 +0530

    Add project files.

---
+++ ProcMeasure/MyProcess.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+
+namespace ProcMeasure
+{
+
+    public class ProcModel
+    {
+        public long MemUsage { get; set; }
+        public string ProcName { get; set; }
+        public Process Proc { get; set; }
+    }
+    class MyProcess
+    {
+        // Opens the Internet Explorer application.
+        public void OpenApplication(string myFavoritesPath)
+        {
+            // Start Internet Explorer. Defaults to the home page.
+            Process.Start("IExplore.exe");
+
+            // Display the contents of the favorites folder in the browser.
+            Process.Start(myFavoritesPath);
+        }
+
+        // Opens urls and .html documents using Internet Explorer.
+        public void OpenWithArguments()
+        {
+            // url's are not considered documents. They can only be opened
+            // by passing them as arguments.
+            Process.Start("IExplore.exe", "www.northwindtraders.com");
+
+            // Start a Web page using a browser associated with .html and .asp files.
+            Process.Start("IExplore.exe", "C:\\myPath\\myFile.htm");
+            Process.Start("IExplore.exe", "C:\\myPath\\myFile.asp");
+        }
+
+        // Uses the ProcessStartInfo class to start new processes,
+        // both in a minimized mode.
+        public void OpenWithStartInfo()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+
+            Process.Start(startInfo);
+
+            startInfo.Arguments = "www.northwindtraders.com";
+
+            Process.Start(startInfo);
+        }
+    }
+    }
---
+++ ProcMeasure/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProcMeasure;
+
+
+    var proList = System.Diagnostics.Process.GetProcesses();
+    var pmodel = new ProcModel();
+    ProcModel[] pArray = new ProcModel[proList.Length];
+    int i = 0;
+
+//    string cmdArgs = Environment.GetCommandLineArgs()[1];
+    string path = "";
+    Console.WriteLine("Enter output path : ");
+    path=Console.ReadLine();
+//string path = "";
+//Console.WriteLine("Enter output path : ");
+//path=Console.ReadLine(); 
+
+//File.Create(path);
+
+string createText = "Date,Process,MemoryUsed" + Environment.NewLine;
+using (StreamWriter writetext = new StreamWriter(path))
+{
+    writetext.WriteLine("");
+}
+
+foreach (var item in proList)
+    {
+        long totalBytesOfMemoryUsed = item.WorkingSet64;
+        var tmpCls = new ProcModel();
+        tmpCls.MemUsage = totalBytesOfMemoryUsed;
+        tmpCls.ProcName = item.ProcessName;
+        tmpCls.Proc = item;
+        pArray[i] = tmpCls;
+        createText += DateTime.Now+"," + item.ProcessName + "," + totalBytesOfMemoryUsed + Environment.NewLine;
+        i++;
+    }
+
+using (StreamWriter writetext = new StreamWriter(path))
+{
+    writetext.Write(createText);
+}
+
+
+string myFavoritesPath =
+                   Environment.GetFolderPath(Environment.SpecialFolder.Favorites);
+
+    MyProcess myProcess = new MyProcess();
+
+    myProcess.OpenApplication(myFavoritesPath);
+    myProcess.OpenWithArguments();
+    myProcess.OpenWithStartInfo();
+    Console.WriteLine("Hello, World!");
+Console.ReadLine();
