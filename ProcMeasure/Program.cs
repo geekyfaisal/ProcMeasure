f5212060d5e44d060ec1cc1f41b927d20429d53e
Faisal Andrabi <geekyfaisal@outlook.com>
2024-08-12 16:05:16 +0530

    zip2

--- ProcMeasure/Program.cs
+++ ProcMeasure/Program.cs
@@ -2,15 +2,13 @@
 using ProcMeasure;
 
 
-    var proList = System.Diagnostics.Process.GetProcesses();
-    var pmodel = new ProcModel();
-    ProcModel[] pArray = new ProcModel[proList.Length];
-    int i = 0;
-
-//    string cmdArgs = Environment.GetCommandLineArgs()[1];
-    string path = "";
-    Console.WriteLine("Enter output path : ");
-    path=Console.ReadLine();
+var proList = System.Diagnostics.Process.GetProcesses();
+var pmodel = new ProcModel();
+ProcModel[] pArray = new ProcModel[proList.Length];
+int i = 0;
+string path = "";
+Console.WriteLine("Enter output path : ");
+path = Console.ReadLine();
 //string path = "";
 //Console.WriteLine("Enter output path : ");
 //path=Console.ReadLine(); 
@@ -18,36 +16,38 @@ using ProcMeasure;
 //File.Create(path);
 
 string createText = "Date,Process,MemoryUsed" + Environment.NewLine;
-using (StreamWriter writetext = new StreamWriter(path))
-{
-    writetext.WriteLine("");
-}
 
-foreach (var item in proList)
+populateData();
+
+if (path != null)
+{
+    using (StreamWriter writetext = new StreamWriter(path))
     {
-        long totalBytesOfMemoryUsed = item.WorkingSet64;
-        var tmpCls = new ProcModel();
-        tmpCls.MemUsage = totalBytesOfMemoryUsed;
-        tmpCls.ProcName = item.ProcessName;
-        tmpCls.Proc = item;
-        pArray[i] = tmpCls;
-        createText += DateTime.Now+"," + item.ProcessName + "," + totalBytesOfMemoryUsed + Environment.NewLine;
-        i++;
+        writetext.WriteLine("");
+        writetext.Write(createText);
     }
-
-using (StreamWriter writetext = new StreamWriter(path))
-{
-    writetext.Write(createText);
 }
-
 
 string myFavoritesPath =
                    Environment.GetFolderPath(Environment.SpecialFolder.Favorites);
 
-    MyProcess myProcess = new MyProcess();
-
-    myProcess.OpenApplication(myFavoritesPath);
-    myProcess.OpenWithArguments();
-    myProcess.OpenWithStartInfo();
-    Console.WriteLine("Hello, World!");
 Console.ReadLine();
+
+ void populateData()
+{
+    var proList = System.Diagnostics.Process.GetProcesses();
+    var pmodel = new ProcModel();
+    ProcModel[] pArray = new ProcModel[proList.Length];
+    int i = 0;
+    foreach (var item in proList)
+    {
+        long totalBytesOfMemoryUsed = item.WorkingSet64;
+        var tmpCls = new ProcModel();
+        tmpCls.MemUsage = totalBytesOfMemoryUsed;
+        tmpCls.ProcName = item.MainWindowTitle;
+        tmpCls.Proc = item;
+        pArray[i] = tmpCls;
+        createText += DateTime.Now + "," + item.ProcessName + "," + totalBytesOfMemoryUsed +","+ item.MainWindowTitle + Environment.NewLine;
+        i++;
+    }
+}
